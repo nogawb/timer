728fa3d5837112396f18c18b1561523ef4837df1
Benjamin Nogawa <benjamin.nogawa@gmail.com>
2018-02-28 18:19:44 -0800

    The first commit
    first commit

---
+++ timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  timer
+//
+//  Created by Ben Nogawa on 2/28/18.
+//  Copyright © 2018 Ben Nogawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
